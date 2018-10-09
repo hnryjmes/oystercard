2939e8080db2679f60d1abe2a8bd9c2191b184e9
Rupert Adams <RupertNCAdams@aol.com>
2018-10-09 15:30:42 +0100

    journey test expantion

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,3 +1,12 @@
+require_relative 'station'
+
 class Journey
-  
+
+  def entry_station
+    { name: 'Waterloo' }
+  end
+
+  def exit_station
+    
+  end
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -1,5 +1,11 @@
 require 'journey'
 
 describe Journey do
-  
+  let (:station) { double :station }
+  let (:station_2) { double :station }
+  let(:journey){ {from: station, to: station_2} }
+
+  it 'can store entry station' do
+    expect(subject.entry_station[:name]).to eq 'Waterloo'
+  end
 end
