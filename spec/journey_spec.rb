376425699fe704c6120271d7933b8892e890f321
Rupert Adams <RupertNCAdams@aol.com>
2018-10-09 16:19:26 +0100

    fixed journey test

--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -1,16 +1,18 @@
 require 'journey'
 
 describe Journey do
+
   let (:station) { double :station }
   let (:station_2) { double :station }
-  let (:journey) { {from: station, to: station_2} }
+  subject {described_class.new(station, station_2)}
 
-  # it 'can store entry station' do
-  #   expect(subject.entry_station[:name]).to include 'Waterloo'
-  # end
-  #
-  # it 'can store exit station' do
-  #   expect(subject.exit_station[:name]).to include 'Old Street'
-  # end
+
+  it 'can store entry station' do
+    expect(subject.from).to eq station
+  end
+
+  it 'can store exit station' do
+    expect(subject.to).to eq station_2
+  end
 
 end
